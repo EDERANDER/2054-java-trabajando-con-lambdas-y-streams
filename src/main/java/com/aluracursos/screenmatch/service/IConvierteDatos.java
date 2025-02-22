6fc5d01ff9189b1bcad6d5be1b50627e980daf19
EDER ANDER <88508026+EDERANDER@users.noreply.github.com>
2025-02-22 06:51:27 -0500

    Update IConvierteDatos.java
    docs: agregando documentacion al metodo obtenerDatos() de la clase Interface IConvierteDatos.

--- src/main/java/com/aluracursos/screenmatch/service/IConvierteDatos.java
+++ src/main/java/com/aluracursos/screenmatch/service/IConvierteDatos.java
@@ -1,5 +1,16 @@
 package com.aluracursos.screenmatch.service;
 //CREADA EN EL VIDEO 1.4
 public interface IConvierteDatos {
+      
+    /**
+     * Convierte un JSON en un objeto del tipo especificado.
+     *
+     * @param <T>    Indica que el método es genérico y puede trabajar con cualquier tipo T.
+     * @param json   Una cadena JSON de la que queremos extraer datos.
+     * @param clase  La clase en la que queremos convertir el JSON (ejemplo: Usuario.class), 
+                     tambien significa que puede ingresar cualquier clase por eso es Class<T> (generica).
+     * @return       Un objeto del tipo T, que se decide al momento de la llamada.
+     * @throws IllegalArgumentException Si el JSON no se puede convertir al tipo T.
+     */
     <T> T obtenerDatos(String json, Class<T> clase);
 }
